c2f9b634cbd1f602dee2f9ea50804af71cdc3c9d
Ruslan Gadaborshev <qa.gadaborshev@gmail.com>
2023-09-03 11:50:30 +0300

    Refactored code of  project of Day 3 - Treasure island

--- Day_3/project_treasure_island.py
+++ Day_3/project_treasure_island.py
@@ -11,12 +11,11 @@ print('''
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.") 
 
-direction = input("You're at a cross road. Where do you want to go? Type \"left\" or \"right\"\n")
+direction = input("You're at a cross road. Where do you want to go? Type \"left\" or \"right\"\n").lower()
 if direction == "left":
-    action = input("You've come to a lake. There is an island in the middle of the lake. Type \"wait\" to wait for a boat. Type \"swim\" to swim across\n")
-    # action = "wait" or "swim"
+    action = input("You've come to a lake. There is an island in the middle of the lake. Type \"wait\" to wait for a boat. Type \"swim\" to swim across\n").lower()
     if action == "wait":
-        door = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?\n")
+        door = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?\n").lower()
         if door == "red":
             print("It's a room full of fire. Game over!")
         elif door == "blue":
